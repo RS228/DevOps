fd2b3385c8c061ab62d184e06853a293589a9a97
roman-sylchuk-cloudeou <adrobson13@gmail.com>
2023-01-13 22:58:23 +0200

    lab4

--- DevOps/Lab4/jenkins/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-kubectl create ns jenkins
-kubectl apply -f ./manifests
-kubectl create configmap jenkins-config --from-file=./config/jenkins.yaml --from-file=./config/plugins.txt --namespace jenkins
